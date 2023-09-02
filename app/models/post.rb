c05eed7662d9706242381ca467bc267903339501
santodev <dseosantos76@gmail.com>
2023-09-02 06:33:23 -0400

    Post model added and inherit from User

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+	validates :title, :body, :user_id, presence: true
+	belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	validates :email, :username, :password, presence: true
 	validates :username, length: {in: 4..12}
-	validates :password, length: {in: 6..16}
+    validates :password, length: {in: 6..16}
+    has_many :posts
 end
